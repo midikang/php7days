66430c10ee26b24f95b49a8b2a03e3b0d474b9a5
Midi Kang <midikang@t3ex-group.com>
2019-03-31 18:55:22 +0800

    2

---
+++ array2.php
@@ -0,0 +1,18 @@
+<?php
+
+// 用[]定义数组
+$minren = [
+  '杨幂',
+  '王珞丹',
+  100 => '刘亦菲',
+  '黄圣依',
+  '范冰冰'
+];
+var_dump($minren);
+
+
+$shu = array(1 , 1.5 , true ,'天王盖地虎，小鸡炖蘑菇');
+
+echo '<pre>';
+var_dump($shu);
+echo '</pre>';
